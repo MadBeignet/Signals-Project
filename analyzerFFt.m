703fb490932037a6bb9f048fd999fcab7aae84f7
Maddie Wisinski <67487326+MadBeignet@users.noreply.github.com>
2022-12-15 16:05:33 -0600

    Delete analyzerFFt.m

--- analyzerFFt.m
+++
@@ -1,7 +0,0 @@
-while true
-    recordblocking(recorder,.2);
-    audioarray = getaudiodata(recorder);
-     data_fft = fft(audioarray);
-    [maxS, indexMax] = max(abs(data_fft(:,1)));
-    indexMax
-end
